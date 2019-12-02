e0320517824c17575064feda358b1c0f0908e1d3
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2019-12-02 17:43:46 +0700

    activity_data_pemesan: penambahan menu data pemesan
    - Dimana user dapat melihat data pesanan dia yang sudah dipesan pada form pengajuan proposal, sebagai data pesanan (status)
    
    HECK!!!
    
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

---
+++ app/src/main/java/id/hana/mandorin/GetDataPemesanAdapter.java
@@ -0,0 +1,110 @@
+package id.hana.mandorin;
+
+public class GetDataPemesanAdapter {
+
+    public String id, nik, nama, email, no_hp, alamat, tgl_survey, jenis_borongan, data_desain, data_keterangan, status, nik_mandor, nama_mandor;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id= id;
+    }
+
+    public String getNik() {
+        return nik;
+    }
+
+    public void setNik(String nik) {
+        this.nik= nik;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama= nama;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email= email;
+    }
+
+    public String getNo_hp() {
+        return no_hp;
+    }
+
+    public void setNo_hp(String no_hp) {
+        this.no_hp= no_hp;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat= alamat;
+    }
+
+    public String getTgl_survey() {
+        return tgl_survey;
+    }
+
+    public void setTgl_survey(String tgl_survey) {
+        this.tgl_survey= tgl_survey;
+    }
+
+    public String getJenis_borongan() {
+        return jenis_borongan;
+    }
+
+    public void setJenis_borongan(String jenis_borongan) {
+        this.jenis_borongan= jenis_borongan;
+    }
+
+    public String getData_desain() {
+        return data_desain;
+    }
+
+    public void setData_desain(String data_desain) {
+        this.data_desain= data_desain;
+    }
+
+    public String getData_keterangan() {
+        return data_keterangan;
+    }
+
+    public void setData_keterangan(String data_keterangan) {
+        this.data_keterangan= data_keterangan;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status= status;
+    }
+
+    public String getNik_mandor() {
+        return nik_mandor;
+    }
+
+    public void setNik_mandor(String nik_mandor) {
+        this.nik_mandor= nik_mandor;
+    }
+
+    public String getNama_mandor() {
+        return nama_mandor;
+    }
+
+    public void setNama_mandor(String nama_mandor) {
+        this.nama_mandor= nama_mandor;
+    }
+}
---
+++ app/src/main/java/id/hana/mandorin/recycler_view_data_pemesan.java
@@ -0,0 +1,77 @@
+package id.hana.mandorin;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class recycler_view_data_pemesan extends RecyclerView.Adapter<recycler_view_data_pemesan.ViewHolder> {
+
+    Context context;
+
+    List<GetDataPemesanAdapter> getDataPemesanAdapter;
+
+    public recycler_view_data_pemesan(List<GetDataPemesanAdapter> getDataPemesanAdapter, Context context){
+
+        super();
+        this.getDataPemesanAdapter= getDataPemesanAdapter;
+        this.context = context;
+    }
+
+    @Override
+    public recycler_view_data_pemesan.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_data_pemesan, parent, false);
+
+        recycler_view_data_pemesan.ViewHolder viewHolder = new recycler_view_data_pemesan.ViewHolder(v);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final recycler_view_data_pemesan.ViewHolder Viewholder, int position) {
+
+        GetDataPemesanAdapter getRenovasiDataAdapter1 =  getDataPemesanAdapter.get(position);
+
+        Viewholder.Nik.setText(getRenovasiDataAdapter1.getNik());
+        Viewholder.Alamat.setText(getRenovasiDataAdapter1.getAlamat());
+        Viewholder.Nama.setText(getRenovasiDataAdapter1.getNama());
+        Viewholder.Alamat.setText(getRenovasiDataAdapter1.getAlamat());
+        Viewholder.Status.setText(getRenovasiDataAdapter1.getStatus());
+    }
+
+    @Override
+    public int getItemCount() {
+        return getDataPemesanAdapter.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public TextView Nik, Alamat, Nama, Status, Tgl_survey;
+        public CardView cv_head;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            itemView.setClickable(true);
+            itemView.setOnClickListener(this);
+
+            Nik = itemView.findViewById(R.id.text_id_data_pemesan_1);
+            Alamat = itemView.findViewById(R.id.alamat_pemesan_1);
+            Nama = itemView.findViewById(R.id.nomor_pemesan_data_pemesan_1);
+            Status = itemView.findViewById(R.id.status_pemesan_1);
+            Tgl_survey = itemView.findViewById(R.id.tgl_survey_pemesan_1);
+        }
+
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+}
