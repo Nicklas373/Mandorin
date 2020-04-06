de5739b96cea397cb2e32b2039bbd1075fdcc0d0
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2020-04-06 18:21:39 +0700

    treewide: activity_register: Desain baru :3
    * Beberapa layout register mendapatkan desain layout baru dan perbaikan beberapa layout
    * Penghapusan tombol lihat akun dan beranda pada activity_register_5, pengguna sekarang akan di arahkan langsung ke beranda ketika menyelesaikan pendaftaran
    
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- app/src/main/java/id/hana/mandorin/activity_register_5.java
+++ app/src/main/java/id/hana/mandorin/activity_register_5.java
@@ -3,13 +3,10 @@ package id.hana.mandorin;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
 public class activity_register_5 extends AppCompatActivity {
-
-    private Button beranda, account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,23 +14,13 @@ public class activity_register_5 extends AppCompatActivity {
         setContentView(R.layout.activity_register_5);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        beranda = (Button) findViewById(R.id.btn_beranda);
-        account = (Button) findViewById(R.id.btn_account);
-
-        beranda.setOnClickListener(new View.OnClickListener() {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
+            public void run() {
                 Intent intent = new Intent(activity_register_5.this, MainActivity.class);
                 startActivity(intent);
             }
-        });
-
-        account.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(activity_register_5.this, activity_user_profile.class);
-                startActivity(intent);
-            }
-        });
+        }, 2000L); //2000 L = 2 detik
     }
 }
